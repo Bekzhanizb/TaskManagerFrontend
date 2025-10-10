be962e0fa5575ac43c330ce523a017075dd53255
Zhenis <zenismederbek@gmail.com>
2025-10-10 22:07:41 +0500

    fixed errors

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:8000/api', // ⚠️ Замените при необходимости
+    baseURL: 'http://localhost:8000/api',
 });
 
 export const fetchTasks = () => API.get('/tasks');
